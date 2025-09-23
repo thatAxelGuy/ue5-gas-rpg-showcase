5cb68928b4721decc955ce64ad4ea9d26d302132
Axel Woermann <53866066+AxlMan31@users.noreply.github.com>
2025-09-23 13:48:54 +0200

    feat: set up derived character and enemy classes

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,17 @@
+// Copyright Axel Woermann
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -0,0 +1,5 @@
+// Copyright Axel Woermann
+
+
+#include "Character/AuraEnemy.h"
+
