cb49be8a6742de29bb026f8606cfc382901ee8fa
Axel Woermann <53866066+AxlMan31@users.noreply.github.com>
2025-09-24 15:50:45 +0200

    feat: Set up pointers to UAbilitySystemComponent and UAttributeSet in PlayerState and Enemy files

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -3,7 +3,19 @@
 
 #include "Player/AuraPlayerState.h"
 
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
 AAuraPlayerState::AAuraPlayerState()
 {
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true);
+	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>(TEXT("AttributeSet"));
+	
 	SetNetUpdateFrequency(100.f);
+}
+
+UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
 }
--- Source/AuraEditor.Target.cs
+++ Source/AuraEditor.Target.cs
@@ -9,6 +9,7 @@ public class AuraEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
 
 		ExtraModuleNames.AddRange( new string[] { "Aura" } );
 	}
