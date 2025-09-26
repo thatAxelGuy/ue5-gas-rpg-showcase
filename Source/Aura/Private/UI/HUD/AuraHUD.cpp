6b9aff51c88877507c5859154c5c9200c30483b2
Axel Woermann <53866066+AxlMan31@users.noreply.github.com>
2025-09-26 16:29:08 +0200

    feat: create BP_OverlayWidgetController and bind to OnHealthChanged an OnMaxHealthChanged Events in WBP_HealthGlobe

--- Source/Aura/Private/UI/HUD/AuraHUD.cpp
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -28,11 +28,10 @@ void AAuraHUD::InitOverlay(APlayerController* PC, APlayerState* PS, UAbilitySyst
 	OverlayWidget = Cast<UAuraUserWidget>(Widget);
 
 	const FWidgetControllerParams WidgetControllerParams(PC, PS, ASC, AS);
-
 	UOverlayWidgetController* WidgetController = GetOverlayWidgetController(WidgetControllerParams);
 
 	OverlayWidget->SetWidgetController(WidgetController);
-	
+	WidgetController->BroadcastInitialValues();	
 	Widget->AddToViewport();
 }
 
