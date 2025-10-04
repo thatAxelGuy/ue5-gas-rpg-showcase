83af62473426ed609dc45b0280682d694344738a
Axel Woermann <53866066+AxlMan31@users.noreply.github.com>
2025-10-04 17:55:43 +0200

    feat: experiment with duration based gameplay effect increasing max health for 2 seconds

--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -22,8 +22,14 @@ protected:
 
 	UFUNCTION(BlueprintCallable)
 	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass);
+	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Applied Effects")
 	TSubclassOf<UGameplayEffect>InstantGameplayEffectClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Applied Effects")
+	TSubclassOf<UGameplayEffect>DurationGameplayEffectClass;
+
+	
 
 private:
 	
