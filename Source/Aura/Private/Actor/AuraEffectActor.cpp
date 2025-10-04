1e548aa8e8167933f758fdde4c5f5266db958a44
Axel Woermann <53866066+AxlMan31@users.noreply.github.com>
2025-10-04 19:08:34 +0200

    feat: create infinite GE GE_FireArea -prepare for infinite effect removal -set up enums for application and removal policies

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -34,3 +34,11 @@ void AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGam
 	TargetAbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
 }
 
+void AAuraEffectActor::OnOverlap(AActor* TargetActor)
+{
+}
+
+void AAuraEffectActor::OnEndOverlap(AActor* TargetActor)
+{
+}
+
