10e5218f22c40749f6947af6de77a0b71737cda6
Axel Woermann <53866066+AxlMan31@users.noreply.github.com>
2025-10-26 11:41:55 +0100

    chore: Experimenting with order of operations on test Gameplay Effect Attribute Based

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -10,9 +10,9 @@
 
 UAuraAttributeSet::UAuraAttributeSet()
 {
-	InitHealth(50.f);
+	InitHealth(10.f);
 	InitMaxHealth(100.f);
-	InitMana(25.f);
+	InitMana(10.f);
 	InitMaxMana(50.f);
 }
 
@@ -38,12 +38,12 @@ void UAuraAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, 
 
 	if (Attribute == GetHealthAttribute())
 	{
-		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxHealth());
+		//NewValue = FMath::Clamp(NewValue, 0.f, GetMaxHealth());
 	}
 
 	if (Attribute == GetManaAttribute())
 	{
-		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxMana());
+		//NewValue = FMath::Clamp(NewValue, 0.f, GetMaxMana());
 	}
 }
 
@@ -103,11 +103,11 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const struct FGameplayEffectMo
 
 	if(Data.EvaluatedData.Attribute == GetHealthAttribute())
 	{    
-		SetHealth(FMath::Clamp(GetHealth(), 0.f, GetMaxHealth()));
+		//SetHealth(FMath::Clamp(GetHealth(), 0.f, GetMaxHealth()));
 	}
 	if(Data.EvaluatedData.Attribute == GetManaAttribute())
 	{
-		SetMana((FMath::Clamp(GetMana(), 0.f, GetMaxMana())));
+		//SetMana((FMath::Clamp(GetMana(), 0.f, GetMaxMana())));
 	}
 }
 
