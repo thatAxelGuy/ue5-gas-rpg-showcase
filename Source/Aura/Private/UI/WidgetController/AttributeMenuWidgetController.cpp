0a7b64a9ea185533db4bd92fb6c1e38d323f92c9
Axel Woermann <53866066+AxlMan31@users.noreply.github.com>
2026-01-06 10:27:27 +0100

    Add functionality to set AuraAttributeWidgetController

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,12 @@
+// Copyright Axel Woermann
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+}
