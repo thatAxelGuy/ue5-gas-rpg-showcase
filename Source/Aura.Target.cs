d94d5f94456216ac38c4c61378352fde2d9c7b8d
Axel Woermann <53866066+thatAxelGuy@users.noreply.github.com>
2026-04-21 15:06:33 +0200

    Update project to UE 5.7

--- Source/Aura.Target.cs
+++ Source/Aura.Target.cs
@@ -8,7 +8,7 @@ public class AuraTarget : TargetRules
 	public AuraTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 
 		ExtraModuleNames.AddRange( new string[] { "Aura" } );
 	}
