20c1d24e393093e30d3624032924846e23ac3ca9
Axel Woermann <53866066+thatAxelGuy@users.noreply.github.com>
2026-04-28 17:03:12 +0200

    Add hit react system and startup abilities
    Introduce hit-react gameplay flow and startup ability granting. Adds hit react assets (montages, GA_HitReact, GE_HitReact) and updates DA_CharacterClassInfo to include CommonAbilities. New GameplayTag Effects.HitReact is registered and UAuraAttributeSet triggers activation of hit-react abilities when damage is non-fatal. UAuraAbilitySystemLibrary::GiveStartupAbilities grants class common abilities at BeginPlay; AuraEnemy initializes abilities, registers the hit react tag event, and toggles bHitReacting while reacting. Exposes HitReact montage via CombatInterface and AAuraCharacterBase, and adds HitReactMontage property to characters.

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -9,6 +9,8 @@
 #include "Aura/Aura.h"
 #include "Components/WidgetComponent.h"
 #include "UI/Widget/AuraUserWidget.h"
+#include "AuraGameplayTags.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 
 AAuraEnemy::AAuraEnemy()
@@ -48,7 +50,9 @@ int32 AAuraEnemy::GetPlayerLevel()
 void AAuraEnemy::BeginPlay()
 {
 	Super::BeginPlay();
+	GetCharacterMovement()->MaxWalkSpeed = BaseWalkSpeed;
 	InitAbilityActorInfo();
+	UAuraAbilitySystemLibrary::GiveStartupAbilities(this, AbilitySystemComponent);
 
 	if (UAuraUserWidget* AuraUserWidget = Cast<UAuraUserWidget>(HealthBar->GetUserWidgetObject()))
 	{
@@ -70,10 +74,20 @@ void AAuraEnemy::BeginPlay()
 				OnMaxHealthChanged.Broadcast(Data.NewValue);
 			}
 		);
+		AbilitySystemComponent->RegisterGameplayTagEvent(FAuraGameplayTags::Get().Effects_HitReact, EGameplayTagEventType::NewOrRemoved).AddUObject(
+				this,
+				&AAuraEnemy::HitReactTagChanged
+				);
 		
 		OnHealthChanged.Broadcast(AuraAS->GetHealth());
 		OnMaxHealthChanged.Broadcast(AuraAS->GetMaxHealth());
 	}
+}
+
+void AAuraEnemy::HitReactTagChanged(const FGameplayTag CallbackTag, int32 NewCount)
+{
+	bHitReacting = NewCount > 0;
+	GetCharacterMovement()->MaxWalkSpeed = bHitReacting ? 0.f : BaseWalkSpeed;
 }
 
 void AAuraEnemy::InitAbilityActorInfo()
